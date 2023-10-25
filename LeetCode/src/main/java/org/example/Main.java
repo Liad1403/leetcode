829154cc27ff179ba6a6a363d8c0a7294ca44338
Liad1403 <115228691+Liad1403@users.noreply.github.com>
2023-10-25 10:32:23 +0300

    liad_branch first commit

--- LeetCode/src/main/java/org/example/Main.java
+++ LeetCode/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static int[] Sum(int [] array1,int [] array2,int [] array3)
     {
+        print("liad!!");
         array3.sort();
         int  array[]=new int[];
         for (int i=0;i<array1.length();i++)
